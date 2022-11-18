0759498243b5d5b049dbc0978e907d06161f1a19
Jerry Dai <jdai@lcs.on.ca>
2022-11-18 15:11:02 -0500

    Finished the circle 2

--- ShrinkingCircles/ContentView.swift
+++ ShrinkingCircles/ContentView.swift
@@ -26,10 +26,43 @@ struct ShrinkingCircles: Shape {
     
 }
 
+struct CircleTwo : Shape
+{
+    func drawCircles(path: inout Path, center:CGPoint, radius: Double, level:Int){
+        if(level == 0)
+        {
+            return
+        }
+        
+        path.addEllipse(in: CGRect(origin: CGPoint(x: center.x - radius, y: center.y - radius ), size: CGSize(width: radius * 2, height: radius * 2)))
+        
+        
+        var points: [CGPoint] =  [
+            CGPoint(x: center.x - radius, y: center.y),
+            CGPoint(x: center.x + radius, y: center.y),
+            CGPoint(x: center.x , y: center.y + radius),
+            CGPoint(x: center.x , y: center.y - radius),
+        ]
+        
+        for _point in points
+        {
+            drawCircles(path: &path, center: _point, radius: radius / 3, level: level - 1)
+        }
+
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        drawCircles(path: &path, center: CGPoint(x: rect.maxX / 2, y: rect.maxY / 2 ), radius: 200, level: 4)
+        return path
+    }
+    
+    
+}
 
 struct ContentView: View {
     var body: some View {
-        ShrinkingCircles()
+        CircleTwo()
             .stroke()
     }
 }
